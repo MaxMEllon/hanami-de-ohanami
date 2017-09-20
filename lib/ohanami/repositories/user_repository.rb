67c88902cd19daf5c48402c3ce0d46ea862c0184
MaxMEllon <maxmellon1994@gmail.com>
2017-09-20 09:00:17 +0900

    Refactoring

--- lib/ohanami/repositories/user_repository.rb
+++ lib/ohanami/repositories/user_repository.rb
@@ -2,14 +2,21 @@
 
 class UserRepository < Hanami::Repository
   def find_by_email!(email)
-    user = users.where(email: email).first
-    return user if user
-    raise Ohanami::RecordNotFoundException.new('User not found by email')
+    find_by_email_base(email, true)
+  rescue
+    $!
   end
 
   def find_by_email(email)
+    find_by_email_base(email)
+  end
+
+  private
+
+  def find_by_email_base(email, force = false)
     user = users.where(email: email).first
     return user if user
+    raise Ohanami::RecordNotFoundException.new('User not found by email') if force
     false
   end
 end
